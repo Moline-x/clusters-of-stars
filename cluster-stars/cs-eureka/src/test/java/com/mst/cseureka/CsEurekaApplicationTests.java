2502ef73950cf0ff21156c03e460532a591de1ba
常墨林 <2223745240@qq.com>
2021-11-14 20:05:33 +0800

    新建eureka服务发现中心

---
+++ cluster-stars/cs-eureka/src/test/java/com/mst/cseureka/CsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mst.cseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
